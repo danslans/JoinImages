0db174b1ae2a7dceac1db326c7759397d3a6b37f
Daniel Gomez <daniel.gomez@imagemaker.com>
2021-07-27 21:57:06 -0500

    agrega avances

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 window.addEventListener("load",function (event) {
     
     let contentImg=document.getElementById("contentImgs");
+    
     let canvas = document.getElementById("canvas");
+    canvas.style.width = "100%";
+    canvas.style.backgroundColor ="red";
+    let context=canvas.getContext("2d");
+    let height = [];
+    
     this.document.addEventListener("change",(eventChange)=>{
         contentImg.innerHTML="";
         Array.from(eventChange.target.files)
         .forEach((element,index) => {
+            debugger
             let img = this.document.createElement("img");
-            img.height = "100";
-            img.width = "100";
+            //img.style.width= "50%" ;
+           // img.style.height= "50%" ;
+            img.style.display="none" ;
             img.id = `img${index}`;
             let fileReader = new FileReader();
             fileReader.onloadend =()=>{
                 img.src = fileReader.result;
+                img.onload = function(){
+                    debugger
+                    height.push(parseInt(this.height));
+                    canvas.style.height = `${height.reduce((previous,current)=>previous+current)}px`;
+                }
             }
             fileReader.readAsDataURL(element);
             contentImg.appendChild(img);
